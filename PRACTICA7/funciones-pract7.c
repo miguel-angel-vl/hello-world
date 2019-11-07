9f6871bd93dec787af75b67e06a7093aaa677270
miguel-angel-vl <55257359+miguel-angel-vl@users.noreply.github.com>
2019-11-07 15:56:18 -0400

    Update funciones-pract7.c

--- PRACTICA7/funciones-pract7.c
+++ PRACTICA7/funciones-pract7.c
@@ -6,10 +6,14 @@ void elimoracion(char *oracion){
     int ind;
 
     for(ind = 0; *(oracion+ind); ind++){
-
-        if(*(oracion+ind) == '.' || *(oracion+ind) == '?'){
-            strcpy(oracion, oracion+ind+2);
-            return;
+        
+        if(*(oracion+ind) == '.' || *(oracion+ind) == '?' || *(oracion+ind) == '!'){
+            if(*(oracion+ind) == ' ')
+                strcpy(oracion, oracion+ind+2);
+                return;
+            else
+                strcpy(oracion, oracion+ind+1);
+                return;
         }
     }
     *oracion = NULL;
