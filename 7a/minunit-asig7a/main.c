f2326f6991bac4c95f466634154a40f92b326f30
itactuk <46655567+itactuk@users.noreply.github.com>
2019-11-09 17:28:42 -0400

    ejemplo minunit con malloc

--- 7a/minunit-asig7a/main.c
+++ 7a/minunit-asig7a/main.c
@@ -21,7 +21,7 @@ MU_TEST(test_simetrica2) {
 		{4, 7, 2, 0, 4},
 		{5, 7, 1, 4, 5}
      };
-
+	
 	mu_assert_int_eq(1, simetrica(matval, 5));
 }
 
@@ -31,8 +31,21 @@ MU_TEST(test_simetrica3) {
 		{5, 8, 1},
 		{0, 9, 2},
      };
+	int ** matriz = (int **) malloc(3 * sizeof(int*));
+	for(int i=0; i<3; i++){
+		matriz[i] = (int *) malloc(3*sizeof(int));
+	}
+	matriz[0][0] = 8; matriz[0][1] = 2; matriz[0][2] = 3;
+	matriz[1][0] = 5; matriz[1][1] = 28; matriz[1][2] = 1;
+	matriz[2][0] = 0; matriz[2][1] = 9; matriz[2][2] = 2;
+	
 
-	mu_assert_int_eq(0, simetrica(matval, 3));
+	mu_assert_int_eq(0, simetrica(matriz, 3));
+	
+	for(int i=0; i<3; i++){
+		free(matriz[i]);
+	} 
+	free(matriz);
 }
 
 MU_TEST(test_simetricaError) {
